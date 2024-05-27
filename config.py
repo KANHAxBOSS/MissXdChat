c0116c9e679ae18a2ccbf4a5bd4a265f142f86c3
Danishzain05 <163726214+Danishzain05@users.noreply.github.com>
2024-05-27 07:55:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "6440277281"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "LinkZoneChat_05")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "LinkZone_05")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "LinkZone_05")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "ZainXd_05")
 
